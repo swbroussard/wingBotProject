b6138d1e624c417f3722c6fa2d94417210ba28e8
Steven Broussard <stevenbroussard@MacBook-Pro.local>
2017-04-10 19:44:00 -0700

    edited php file.

--- wingManProject/static/authAndProc.php
+++ wingManProject/static/authAndProc.php
@@ -5,21 +5,12 @@
        HTML and CSS -->
 
 <!DOCTYPE html>
-<head>
-    <script>
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                myObj = JSON.parse(this.responseText);
-                document.getElementById("demo").innerHTML = myObj.name;
-            }
-        };
-        xmlhttp.open("GET", "json_demo.txt", true);
-        xmlhttp.send();
-    </script>
-</head>
 <html>
     <body>
-        <p>Take a look at <a href="json_demo.json" target="_blank">json_demo.txt</a></p>
+        <?php
+            $fileContent = file_get_contents("./some_folder/*.json");
+            $json_content = json_decode($fileContent, true);
+            
+        ?>
     </body>
 </html>
